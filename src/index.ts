8f9c06696ef6a8bb8dd4ee4dd26c3e4e20a5f9ab
neenu <neenukurian92@gmail.com>
2018-04-21 10:36:09 +0200

    setup done

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata'
+import {createKoaServer} from "routing-controllers"
+import Controller from "./controller"
+import setupDb from './db'
+
+
+const app = createKoaServer({
+   controllers: [Controller]
+})
+
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
